5ad44279ef5b5e2e877507f1ec0ed0142becd3e5
bohdan-travianko-uzhnu <travianko.bohdan@student.uzhnu.edu.ua>
2025-03-31 23:13:38 +0300

    Added cool-users page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+// Існуючий маршрут
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// Новий маршрут для cool-users
+router.get('/cool', function(req, res, next) {
+  res.render('cool-users', { title: 'Cool Users' });
 });
 
 module.exports = router;
